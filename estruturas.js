92279e7da777346f564ae7004bd7d364347f1e0b
Ana Beatriz Emiko Morita <anabemorita@gmail.com>
2019-02-21 10:50:23 -0300

    Criar exemplos de javascript

---
+++ estruturas.js
@@ -0,0 +1,104 @@
+//ESTRUTURAS LÓGICAS
+
+//SE SENÃO
+
+/*
+if(condicao){
+    bloco de codigo do if
+}
+else{
+    bloco de codigo do else
+}
+*/
+
+/*
+if(condicao){
+
+}
+else  if (condicao){
+
+}
+else if (condicao){
+
+}
+else if (condicao){
+
+}
+else{
+
+}
+
+
+*/
+
+
+/*
+if(nome começa com A){
+    codigo
+}
+if(nomes tem mais de 3 letras){
+    codigo
+}
+*/
+
+//LAÇOS
+//estruturas repetição
+
+//ENQUANTO 
+//WHILE
+
+// while(condicao1 && condicao2){
+//     codigo do while
+// }
+////ESTRUTURAS LÓGICAS
+
+//SE SENÃO
+
+/*
+if(condicao){
+    bloco de codigo do if
+}
+else{
+    bloco de codigo do else
+}
+*/
+
+/*
+if(condicao){
+
+}
+else  if (condicao){
+
+}
+else if (condicao){
+
+}
+else if (condicao){
+
+}
+else{
+
+}
+
+
+*/
+
+
+/*
+if(nome começa com A){
+    codigo
+}
+if(nomes tem mais de 3 letras){
+    codigo
+}
+*/
+
+//LAÇOS
+//estruturas repetição
+
+//ENQUANTO 
+//WHILE
+
+// while(condicao1 && condicao2){
+//     codigo do while
+// }
---
+++ variaveis.js
@@ -0,0 +1,43 @@
+//DECLARANDO VARIAVEIS
+
+let nomeDaVariavel;
+nomeDavariavel ="texto";
+let nome = "Vanessa";
+let variavel = "string";
+
+const pi = 3.14159;
+
+//TIPOS DE VARIAVEIS
+
+//TEXTO
+let texto = "string";
+
+//NUMERO
+let numero = 2;
+let inteiro = 33;
+let decimal = 32.5;
+
+//BOOLEANO
+let verdade = true;
+let falso = false;
+
+//LISTA
+let lista = [];
+let compras = ["banana", "melancia", "arroz", "suco"];
+let aleatoria = [1, "ana", true, ["estacao hack", 1234, false]];
+
+//OBJETO
+let objeto = {};
+objeto = {
+    "chave": "valor",
+    "nome": "Vanessa",
+    "idade": 24
+};
+
+let listaAlunos = [
+    {"nome": "Ana", "idade": 22},
+    {"nome": "Vanessa", "idade": 24}
+];
+
+
+
